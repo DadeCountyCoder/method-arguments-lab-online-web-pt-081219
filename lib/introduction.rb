37117d6a400c6a28ff937905727a360239f0d6b3
Alfredo Diaz <mail4alfredo@gmail.com>
2019-08-13 16:05:00 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,3 @@
 def introduction(name)
-  puts "Hi, my name is #{name}."
-  "Hi, my name is Dan and I am learnig to program in Ember.js."
+  puts "Hi, my name is Dan and I am learnig to program in Ember.js."
 end
